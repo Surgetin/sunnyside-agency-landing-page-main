be9c72346ddd7e593bc6892ed268738537d5e8b1
Surgetin <65120855+Surgetin@users.noreply.github.com>
2022-05-26 20:27:11 +0200

    mobil version is finished

---
+++ main.js
@@ -0,0 +1,14 @@
+const toggleMenu = document.querySelector(".menu");
+const navItem = document.querySelector(".nav__list");
+
+toggleMenu.addEventListener("click", () => {
+    console.log("valami")
+    toggleMenu.classList.toggle("active");
+    navItem.classList.toggle("active");
+});
+
+document.querySelectorAll(".nav__link").forEach(e => 
+    e.addEventListener("click", () => {
+        toggleMenu.classList.remove("active");
+        navItem.classList.remove("active");
+}));
